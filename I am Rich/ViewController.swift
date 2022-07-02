f43c79a3701a19140d2a7cd9b4031efe1441e36e
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-07-02 13:31:21 +0700

    Add I am Rich project

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bagus Kurnia on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
